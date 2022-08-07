a3802b06c20f744f5b594c7264a8f55000634591
Exaltation <michstan96@gmail.com>
2022-08-07 10:22:58 +0700

    Create Side Navigation Menu -Implement logic onclick option menu to open link -Create toolbar

--- app/src/main/java/com/example/cafelegend/HomeActivity.java
+++ app/src/main/java/com/example/cafelegend/HomeActivity.java
@@ -1,15 +1,33 @@
 package com.example.cafelegend;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 
-public class HomeActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     TextView greetingET;
     String username;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+
     void init(){
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navigationView = findViewById(R.id.nav);
+        toolbar = findViewById(R.id.toolbar);
+
         greetingET = findViewById(R.id.greeting_et);
         Bundle extras = getIntent().getExtras();
         if  (extras != null) {
@@ -17,12 +35,56 @@ public class HomeActivity extends AppCompatActivity {
         }
     }
 
-    @Override
+    void setupDrawer(){
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.open_drawer, R.string.close_drawer);
+
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
         init();
+        setupDrawer();
+
         greetingET.append(" " + username + "!");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.side_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.items:
+                Intent items = new Intent(this, ItemsActivity.class);
+                startActivity(items);
+                break;
+
+            case R.id.find_us:
+                Intent find_us = new Intent(this, FindUsActivity.class);
+                startActivity(find_us);
+                break;
+
+            case R.id.logout:
+                Intent login = new Intent(this, LoginActivity.class);
+                startActivity(login);
+                break;
+        }
+        return false;
     }
 }
